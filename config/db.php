608b1ad3cc72eee697746d21e9496b91b2bb1d27
Peter J. Ford <peterjford@gmail.com>
2015-05-26 16:03:57 -0700

    Delete db.php

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-class dbconnect extends mysqli {
-
-	public function getConnection() {
-		$mysqli = mysqli_connect("<insert your mysql hostname here", "<insert your mysql username here>", "<insert your password here>", "pjs_vehicles");
-		return $mysqli;
-	}
-}
