503e4b7e60208055152eeaf98188e8cd82f01929
peterjford <peterjford@gmail.com>
2015-05-26 15:51:54 -0700

    maint.php - Added maintenance page, needs to be reworked into a class. 2015-05-26 Peter J. Ford

---
+++ maint.php
@@ -0,0 +1,19 @@
+<?php
+	/* 
+	 * Written by Peter J. Ford
+	 * 2015-05-12
+	 */
+	require_once('config/db.php');
+	$pagename = "PJ's Maintenance";
+?>
+<html>
+<head>
+<title><?php echo $pagename; ?></title>
+<link href="config/pjs.css" rel="stylesheet">
+</head>
+<body>
+<h1><a href="index.php">PJ's Vehicle Logs</a></h1>
+<h2><?php echo $pagename; ?></h2>
+
+<a href="blank.html">Blank page</a>
+</body></html>
