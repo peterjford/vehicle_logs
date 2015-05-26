1abede8135d4b901ad5447f06f2e3dddc8b2aae7
peterjford <peterjford@gmail.com>
2015-05-26 16:00:34 -0700

    db.php - Updated login info. 2015-05-26 Peter J. Ford.

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 class dbconnect extends mysqli {
 
 	public function getConnection() {
-		$mysqli = mysqli_connect("127.0.0.1", "root", "suitepay", "pjs_vehicles");
+		$mysqli = mysqli_connect("<insert your mysql hostname here", "<insert your mysql username here>", "<insert your password here>", "pjs_vehicles");
 		return $mysqli;
 	}
 }
